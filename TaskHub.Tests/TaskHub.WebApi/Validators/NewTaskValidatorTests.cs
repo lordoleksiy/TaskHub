0a636664cf0aeadbbd6afc8e6bb143d56c27d547
kovol <kovol@smart-it.com>
2023-12-23 22:34:08 +0200

    feature: tests for validation

---
+++ TaskHub.Tests/TaskHub.WebApi/Validators/NewTaskValidatorTests.cs
@@ -0,0 +1,117 @@
+﻿using FluentValidation.TestHelper;
+using TaskHub.Common.DTO.Task;
+using TaskHub.WebApi.Validators.Task;
+
+namespace TaskHub.Tests.TaskHub.WebApi.Validators
+{
+    [TestFixture]
+    public class NewTaskValidatorTests
+    {
+        private NewTaskValidator _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new NewTaskValidator();
+        }
+
+        [Test]
+        public void Title_WhenEmpty_ShouldHaveValidationError()
+        {
+            // Arrange
+            var task = new NewTaskDTO { Title = "" };
+
+            // Act
+            var result = _validator.TestValidate(task);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(t => t.Title)
+                .WithErrorMessage("Title is required when creating the task.");
+        }
+
+        [Test]
+        public void Title_WhenTooShort_ShouldHaveValidationError()
+        {
+            // Arrange
+            var task = new NewTaskDTO { Title = "short" };
+
+            // Act
+            var result = _validator.TestValidate(task);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(t => t.Title)
+                .WithErrorMessage("The minimum length of title is 6 symbols");
+        }
+
+        [Test]
+        public void Title_WhenTooLong_ShouldHaveValidationError()
+        {
+            // Arrange
+            var task = new NewTaskDTO { Title = new string('a', 256) };
+
+            // Act
+            var result = _validator.TestValidate(task);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(t => t.Title)
+                .WithErrorMessage("Title can be up to 255 characters long.");
+        }
+
+        // Test for DueDate rule
+        [Test]
+        public void DueDate_WhenNull_ShouldHaveValidationError()
+        {
+            // Arrange
+            var task = new NewTaskDTO { DueDate = null };
+
+            // Act
+            var result = _validator.TestValidate(task);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(t => t.DueDate)
+                .WithErrorMessage("DueDate is required");
+        }
+
+        [Test]
+        public void DueDate_WhenInvalidDate_ShouldHaveValidationError()
+        {
+            // Arrange
+            var task = new NewTaskDTO { DueDate = "invalid-date" }; // Assuming DueDate is a string for simplicity
+
+            // Act
+            var result = _validator.TestValidate(task);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(t => t.DueDate)
+                .WithErrorMessage("Invalid Due date!");
+        }
+
+        [Test]
+        public void Description_WhenTooLong_ShouldHaveValidationError()
+        {
+            // Arrange
+            var task = new NewTaskDTO { Description = new string('a', 1001) };
+
+            // Act
+            var result = _validator.TestValidate(task);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(t => t.Description)
+                .WithErrorMessage("Description can be up to 1000 characters long.");
+        }
+
+        [Test]
+        public void ParentTaskId_WhenInvalidGuid_ShouldHaveValidationError()
+        {
+            // Arrange
+            var task = new NewTaskDTO { ParentTaskId = "invalid-guid" }; // Assuming ParentTaskId is a string for simplicity
+
+            // Act
+            var result = _validator.TestValidate(task);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(t => t.ParentTaskId)
+                .WithErrorMessage("Parent task id must be guid.");
+        }
+    }
+}
---
+++ TaskHub.Tests/TaskHub.WebApi/Validators/RegisterModelValidatorTests.cs
@@ -0,0 +1,75 @@
+﻿using FluentValidation.TestHelper;
+using TaskHub.Common.DTO.User;
+using TaskHub.WebApi.Validators.User;
+
+namespace TaskHub.Tests.TaskHub.WebApi.Validators
+{
+    [TestFixture]
+    public class RegisterModelValidatorTests
+    {
+        private RegisterModelValidator _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new RegisterModelValidator();
+        }
+
+        [Test]
+        public void Username_WhenTooShort_ShouldHaveValidationError()
+        {
+            // Arrange
+            var model = new RegisterModel("user", "test@example.com", "Password123");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Username)
+                .WithErrorMessage("Username is too short. Minimum length is 6 symbols.");
+        }
+
+        [Test]
+        public void Password_WhenNotContainingLetters_ShouldHaveValidationError()
+        {
+            // Arrange
+            var model = new RegisterModel("validusername", "test@example.com", "123456");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Password must contain both letters and digits.");
+        }
+
+        [Test]
+        public void Password_WhenNotContainingUppercase_ShouldHaveValidationError()
+        {
+            // Arrange
+            var model = new RegisterModel("validusername", "test@example.com", "password123");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Passwords must have at least one uppercase ('A'-'Z').");
+        }
+
+        [Test]
+        public void Email_WhenInvalid_ShouldHaveValidationError()
+        {
+            // Arrange
+            var model = new RegisterModel("validusername", "invalidemail", "Password123");
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Email)
+                .WithErrorMessage("Invalid email address.");
+        }
+    }
+
+}
